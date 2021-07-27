e6b820636243ee96512ce86661a93a167c26d2e5
spoturno <spoturno16@gmail.com>
2021-07-26 22:44:40 -0300

    voice-mic-detection implemented on main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
-#import speech_recognition as sr
-#import os
-
+import speech_recognition as sr
+import os
 from gtts import gTTS
-gt = gTTS("Hello friend")
-gt.save("friend.mp3")
+
+# from microphone to google voice
+voice = ""
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            audio = r.listen(source)
+            text = r.recognize_google(audio)
+            print(text)
+            if text == "stop":
+                break
+            text = r.recognize_google(audio)
+            voice = voice + str(text)
+        except:
+            print("say something....")
+hr = gTTS(text=voice, lang='en', slow=False)
+hr.save("1.mp3")
 
 
 
